9aeba633405f866b4a828068cdbcd9e5e4fdf8ef
homenko <homenko@protei.ru>
2020-02-27 13:21:30 +0300

    Add CustomList usage in main

--- source/main.cpp
+++ source/main.cpp
@@ -3,26 +3,41 @@
 #include <map>
 #include <set>
 
-#include "allocator.h"
+#include "Allocator.h"
+#include "CustomList.h"
 
-constexpr size_t factorial(int n) {
-    return n == 0 ? 1 : (n * factorial(n - 1));
+constexpr size_t factorial(int n)
+{
+  return n == 0 ? 1 : (n * factorial(n - 1));
 }
 
-int main(int, char *[]) {
-    std::map<int, int> map_with_default_alloc;
-    for (int i = 0; i < 10; ++i) {
-        map_with_default_alloc[i] = i;
-    }
+int main(int, char* [])
+{
+  std::cout << "map with default allocator" << std::endl;
+  std::map<int, int> map_with_default_alloc;
+  for (int i = 0; i < 10; ++i) {
+    map_with_default_alloc[i] = i;
+  }
 
-    std::map<int, int, std::less<>, CustomAllocator<std::pair<int, int>, 10>> map_with_new_alloc{};
-    for (int i = 0; i < 10; ++i) {
-        map_with_new_alloc[i] = factorial(i);
-    }
+  std::cout << "map with custom allocator" << std::endl;
+  std::map<int, int, std::less<>, CustomAllocator<std::pair<int, int>, 10>> map_with_new_alloc{};
+  for (int i = 0; i < 10; ++i) {
+    map_with_new_alloc[i] = factorial(i);
+  }
 
-    for (const auto&[key, val]: map_with_new_alloc) {
-        std::cout << key << ' ' << val << std::endl;
-    }
+  for (const auto&[key, val]: map_with_new_alloc) {
+    std::cout << key << ' ' << val << std::endl;
+  }
 
-    return 0;
+  std::cout << "custom list with custom allocator" << std::endl;
+  CustomList<int, CustomAllocator<int>> custom_list;
+  for (int i = 0; i < 10; ++i) {
+    custom_list.push_front(factorial(i));
+  }
+
+  for (auto elem: custom_list) {
+    std::cout << elem << std::endl;
+  }
+
+  return 0;
 }
